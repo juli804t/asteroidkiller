ef4ef08b09afc4c2e7dbc4caa8b9b814836a37ce
juli804t <70595873+juli804t@users.noreply.github.com>
2020-09-02 16:43:38 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,70 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 6 6 6 6 . . . . . . 
+        . . . . 6 6 6 5 5 6 6 6 . . . . 
+        . . . 7 7 7 7 6 6 6 6 6 6 . . . 
+        . . 6 7 7 7 7 8 8 8 1 1 6 6 . . 
+        . . 7 7 7 7 7 8 8 8 1 1 5 6 . . 
+        . 6 7 7 7 7 8 8 8 8 8 5 5 6 6 . 
+        . 6 7 7 7 8 8 8 6 6 6 6 5 6 6 . 
+        . 6 6 7 7 8 8 6 6 6 6 6 6 6 6 . 
+        . 6 8 7 7 8 8 6 6 6 6 6 6 6 6 . 
+        . . 6 8 7 7 8 6 6 6 6 6 8 6 . . 
+        . . 6 8 8 7 8 8 6 6 6 8 6 6 . . 
+        . . . 6 8 8 8 8 8 8 8 8 6 . . . 
+        . . . . 6 6 8 8 8 8 6 6 . . . . 
+        . . . . . . 6 6 6 6 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, Spaceship1, 0, -70)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.fire, 100)
+    sprite.destroy(effects.trail, 500)
+})
+let Astroid: Sprite = null
+let projectile: Sprite = null
+let Spaceship1: Sprite = null
+Spaceship1 = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 3 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 3 . . . . . . . 
+    . . . . . . 8 8 1 a . . . . . . 
+    . . . . . . 8 3 1 a . . . . . . 
+    . . . . . c c c a a a . . . . . 
+    . . . . 8 8 3 3 3 1 a a . . . . 
+    . . 8 f f f c c a a f f a a . . 
+    . 8 8 8 8 a a 3 3 3 3 1 3 a a . 
+    8 8 8 8 8 8 a a 3 3 3 1 3 3 a a 
+    8 8 8 8 8 8 a a 3 3 3 3 1 3 a a 
+    `, SpriteKind.Player)
+controller.moveSprite(Spaceship1, 100, 0)
+Spaceship1.setFlag(SpriteFlag.StayInScreen, true)
+Spaceship1.setPosition(75, 105)
+game.onUpdateInterval(1000, function () {
+    Astroid = sprites.create(img`
+        . . . . . . . . . c c 8 . . . . 
+        . . . . . . 8 c c c f 8 c c . . 
+        . . . c c 8 8 f c a f f f c c . 
+        . . c c c f f f c a a f f c c c 
+        8 c c c f f f f c c a a c 8 c c 
+        c c c b f f f 8 a c c a a a c c 
+        c a a b b 8 a b c c c c c c c c 
+        a f c a a b b a c c c c c f f c 
+        a 8 f c a a c c a c a c f f f c 
+        c a 8 a a c c c c a a f f f 8 a 
+        . a c a a c f f a a b 8 f f c a 
+        . . c c b a f f f a b b c c 6 c 
+        . . . c b b a f f 6 6 a b 6 c . 
+        . . . c c b b b 6 6 a c c c c . 
+        . . . . c c a b b c c c . . . . 
+        . . . . . c c c c c c . . . . . 
+        `, SpriteKind.Enemy)
+    Astroid.setPosition(randint(0, scene.screenWidth()), 0)
+    Astroid.ay = 100
+})
